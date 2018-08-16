918630a5b889ba3a35282466ead7fc3498dd7854
John Sanford <jpsanford@comcast.net>
2018-08-16 10:23:36 -0400

    Initial code.

---
+++ commonTest3.c
@@ -0,0 +1,28 @@
+/* source module: commonTest3.c */
+
+void commonTest3Func (void)
+{
+    
+}
+
+void commonTest3Func2 (void)
+{
+    
+}
+
+void commonTest3Func3 (void)
+{
+    
+}
+
+
+void commonTest3Func4 (void)
+{
+    
+}
+
+
+void commonTest3Func5 (void)
+{
+    
+}
---
+++ commonTest3.h
@@ -0,0 +1,10 @@
+/* header file commonTest3.h */
+
+#define COMMON_TEST_3_VALUE_1        (1)
+
+/*** function prototypes **/
+void commonTest3Func (void);
+void commonTest3Func2 (void);
+void commonTest3Func3 (void);
+void commonTest3Func4 (void);
+void commonTest3Func5 (void);
