b0193eb3b47ebb82b033688bae3095d3ef6c666e
John Sanford <jpsanford@comcast.net>
2018-08-16 10:39:50 -0400

    First code changes.

--- commonTest3.c
+++ commonTest3.c
@@ -1,28 +1,56 @@
 /* source module: commonTest3.c */
 
-void commonTest3Func (void)
+#include <stdint.h>
+
+
+uint16_t commonTest3Func (uint16_t initial_value_u16)
 {
-    
+    uint16_t result_u16;
+
+
+    result_u16 = initial_value_u16 + 16;
+
+    return (result_u16);       
 }
 
-void commonTest3Func2 (void)
+uint16_t commonTest3Func2 (uint16_t initial_value_u16)
 {
-    
+    uint16_t result_u16;
+
+
+    result_u16 = initial_value_u16 * 2;
+
+    return (result_u16);           
 }
 
-void commonTest3Func3 (void)
+uint16_t commonTest3Func3 (uint16_t initial_value_u16)
 {
-    
+    uint16_t result_u16;
+
+
+    result_u16 = initial_value_u16 + 42;
+
+    return (result_u16);           
 }
 
 
-void commonTest3Func4 (void)
+uint16_t commonTest3Func4 (uint16_t initial_value_u16)
 {
-    
+    uint16_t result_u16;
+
+
+    result_u16 = initial_value_u16 + 1;
+
+    return (result_u16);           
 }
 
 
-void commonTest3Func5 (void)
+uint16_t commonTest3Func5 (uint16_t initial_value_u16)
 {
-    
+    uint16_t result_u16;
+
+
+    result_u16 = initial_value_u16 * 7;
+
+    return (result_u16);           
 }
